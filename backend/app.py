68af5e0f5beb7f7ce7fba8542148cbc9a05f2a10
toriitorippy <toriikatsuya0707@gmail.com>
2021-08-11 17:31:30 +0900

    app.pyの例を記述

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS
 import random
+from happiness import *
+from recommendation import *
 
 app = Flask(__name__, static_folder="./dist/static", template_folder="./dist")
 CORS(app)
@@ -18,6 +20,24 @@ def random_test():
         'randomNum': random.random()
     }
     return jsonify(response)
+
+@app.route('/get_recommend',methods=["POST"])
+def get_recommend():
+    data=request.get_json()
+    # data形式は以下
+    # {answer:["1","3","5","9",.....,"2"]}
+
+    #以下misyashitaの関数
+    answer = [int(x) for x in data["answer"]] #stringからintへ変換
+    happiness = cal_happiness(answer) # 地域-幸福度配列を返す
+    recommendation = cal_recommendation(happiness) #幸福度が高い地域を降順に出力
+    response = {}
+    for i in range(len(recommendation)):
+        response[i] = {
+            "area": recommendation[i],
+        }
+    return jsonify(response)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ backend/happiness.py
@@ -0,0 +1,5 @@
+# 回答の配列から地域の幸福度を計算
+def cal_happiness(answer):
+    ############## 以下に処理を記述 ##############
+    print(answer)
+    pass
